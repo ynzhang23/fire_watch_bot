6582e32cfc3575c3c7ead872d89961c5fac00b4d
Yining <106020817+ynzhang23@users.noreply.github.com>
2023-09-23 15:09:22 -0500

    Change file into importable module

--- run_mat.py
+++ run_mat.py
@@ -1,23 +1,24 @@
 import matlab.engine
 
-image_path = '../photos/2023-09-23_12-28-31.jpg'
+def run_mat(image_path):
+    # Start MATLAB engine
+    eng = matlab.engine.start_matlab()
 
-# Start MATLAB engine
-eng = matlab.engine.start_matlab()
+    # Run MATLAB function in another folder
+    eng.cd(r'fire_detection_model', nargout=0)
 
-# Run MATLAB function in another folder
-eng.cd(r'fire_detection_model', nargout=0)
+    # Call the MATLAB function with the image as input
+    prediction = eng.predictFire(image_path)
 
-# Call the MATLAB function with the image as input
-prediction = eng.predictFire(image_path)
+    # Close the MATLAB engine
+    eng.quit()
 
-# Print the prediction from MATLAB
-if prediction == 'non_fire_images':
-  print('WE ARE SAFE')
-elif prediction == 'fire_images':
-  print('FIRE FIRE FIRE')
-else:
-  print("ERROR")
+    return prediction
 
-# Close the MATLAB engine
-eng.quit()
+def process_matlab_result(prediction):
+    if prediction == 'non_fire_images':
+        return 'WE ARE SAFE'
+    elif prediction == 'fire_images':
+        return 'FIRE FIRE FIRE'
+    else:
+        return 'ERROR'
