8e0bc49d8ce781decb170a47d24ec05109626a8e
Yining <106020817+ynzhang23@users.noreply.github.com>
2023-09-23 00:25:27 -0500

    Save captured photos to folder photos

--- camera.py
+++ camera.py
@@ -1,5 +1,13 @@
 import cv2
 import keyboard
+from datetime import datetime
+import os
+
+# Define the directory to save photos
+save_directory = "photos"  # Change this to your desired directory
+
+# Create the directory if it doesn't exist
+os.makedirs(save_directory, exist_ok=True)
 
 # Open the webcam
 cap = cv2.VideoCapture(0)
@@ -13,9 +21,13 @@ while True:
 
     # Check if the spacebar key is pressed
     if keyboard.is_pressed("space"):
-        # Save the current frame as an image
-        cv2.imwrite("captured_photo.jpg", frame)
-        print("Photo captured!")
+        # Generate a timestamp string
+        timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        
+        # Save the current frame as an image with the timestamp as the filename in the "photo" folder
+        filename = os.path.join(save_directory, f"{timestamp}.jpg")
+        cv2.imwrite(filename, frame)
+        print(f"Photo captured and saved as {filename}!")
 
     # Check for the 'q' key to exit the loop
     if cv2.waitKey(1) & 0xFF == ord("q"):
