1e3625b84941c9c30961f674b933c93ffcf2122e
Yining <106020817+ynzhang23@users.noreply.github.com>
2023-09-23 13:47:07 -0500

    Change from trial function to predict fire function

--- run_mat.py
+++ run_mat.py
@@ -1,16 +1,23 @@
 import matlab.engine
 
+image_path = '../photos/2023-09-23_12-28-31.jpg'
+
 # Start MATLAB engine
 eng = matlab.engine.start_matlab()
 
 # Run MATLAB function in another folder
-eng.cd(r'matlab/2-PretrainedModels', nargout=0)
+eng.cd(r'fire_detection_model', nargout=0)
 
 # Call the MATLAB function with the image as input
-prediction = eng.predict_image()
+prediction = eng.predictFire(image_path)
 
 # Print the prediction from MATLAB
-print(prediction)
+if prediction == 'non_fire_images':
+  print('WE ARE SAFE')
+elif prediction == 'fire_images':
+  print('FIRE FIRE FIRE')
+else:
+  print("ERROR")
 
 # Close the MATLAB engine
 eng.quit()
